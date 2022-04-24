64576f67f87b355cfd4014f1155a458cd0c5b692
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-04-24 10:30:52 +0900

    feat: Hello world with iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+
 fn main() {
-    println!("Hello, world!");
+    Chip8::run(Settings::default()).unwrap();
+}
+
+struct Chip8 {}
+
+impl Application for Chip8 {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (Self {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Chip8")
+    }
+
+    fn update(
+        &mut self,
+        _message: Self::Message,
+        _clipboard: &mut Clipboard,
+    ) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
